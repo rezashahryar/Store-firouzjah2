a64d3e98ce6d30e4ddc745e752619c06b2ec8b1a
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-10-12 12:47:41 +0330

    create request otp and register for admin panel

--- core/models.py
+++ core/models.py
@@ -1,7 +1,21 @@
+from uuid import uuid4
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import validate_integer
 # Create your models here.
 
 
-class User(models.Model):
+class User(AbstractUser):
     ...
+
+
+class RequestOtp(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, unique=True)
+    mobile = models.CharField(max_length=11, validators=[validate_integer])
+    otp_code = models.CharField(max_length=10)
+
+    valid_from = models.DateTimeField(auto_now_add=True)
+    valid_until = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.mobile}: {self.otp_code}'
