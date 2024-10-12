1f0736aec8c743807f466e967ca1767b4f9aa9bc
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-10-12 13:21:07 +0330

    create model category, sub category, base product & register these models for panel admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+
+@admin.register(models.ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.ProductSubCategory)
+class ProductSubCategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.BaseProduct)
+class BaseProductAdmin(admin.ModelAdmin):
+    ...
--- store/models.py
+++ store/models.py
@@ -1,3 +1,45 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    image = models.ImageField(upload_to='store/product-category-images/')
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class ProductSubCategory(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, related_name='sub_categories')
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    image = models.ImageField(upload_to='store/product-subcategory-images/')
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class BaseProduct(models.Model):
+
+    class ProductAuthenticity(models.TextChoices):
+        ORIGINAL = 'org', _('اورجینال')
+    
+    class ProductWarranty(models.TextChoices):
+        HAS = 'h', _('دارد')
+        DOES_NOT_HAVE = 'dh', _('ندارد')
+
+    class ShipingMethod(models.TextChoices):
+        PISHTAAZ = 'pi', _('پیشتاز')
+
+    # store
+    title_farsi = models.CharField(max_length=255)
+    title_english = models.CharField(max_length=255)
+    authenticity = models.CharField(max_length=3, choices=ProductAuthenticity.choices, default=ProductAuthenticity.ORIGINAL)
+    warranty = models.CharField(max_length=2, choices=ProductWarranty.choices, default=ProductWarranty.HAS)
+    shiping_method = models.CharField(max_length=2, choices=ShipingMethod.choices)
+
+    def __str__(self) -> str:
+        return self.title_farsi
