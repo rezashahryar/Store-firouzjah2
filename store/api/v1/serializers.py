2c3fd5ece27d6909df8e813b5abc4c95aa39499b
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-10-14 17:16:10 +0330

    optimization queryset for add column for cover of product in list action

--- store/api/v1/serializers.py
+++ store/api/v1/serializers.py
@@ -56,3 +56,22 @@ class ProductListSerializer(serializers.ModelSerializer):
             'title', 'category', 'slug', 'unit_price', 'discount_percent', 'start_discount_datetime',
             'end_discount_datetime'
         ]
+
+    def to_representation(self, instance):
+        context = super().to_representation(instance)
+
+        images = self.context['images']
+        for img in images:
+            if img.base_product_id == instance.base_product.pk and img.is_cover:
+                img_obj = img
+                context['cover'] = ProductImageSerializer(img_obj).data
+            elif not img.base_product_id == instance.base_product.pk and img.is_cover:
+                context['cover'] = None
+        
+        # try:
+        #     cover = models.ProductImage.objects.get(base_product_id=instance.base_product.pk, is_cover=True)
+        # except models.ProductImage.DoesNotExist:
+        #     cover = None
+        # context['cover'] = ProductImageSerializer(cover).data
+
+        return context
--- store/api/v1/views.py
+++ store/api/v1/views.py
@@ -18,13 +18,32 @@ class ProductCategoryListApiView(generics.ListAPIView):
 class ProductViewSet(mixins.RetrieveModelMixin,
                    mixins.ListModelMixin,
                    GenericViewSet):
-    queryset = models.Product.objects.select_related('base_product').prefetch_related(Prefetch(
-        'base_product__properties',
-        queryset=models.SetProductProperty.objects.select_related('property')
-    )).all()
     lookup_field = 'slug'
+
+    def get_queryset(self):
+        queryset = models.Product.objects.select_related('base_product__category') \
+            .prefetch_related(Prefetch(
+                'base_product__properties',
+                queryset=models.SetProductProperty.objects.select_related('property')
+        )).all()
+
+        if self.action == 'list':
+            return queryset.defer(
+                'inventory', 'unit', 'size','length_package', 'width_package', 'height_package',
+                'weight_package', 'shenaase_kaala', 'barcode', 'product_status', 'active_status',
+                'reason', 'base_product__title_english', 'base_product__product_code',
+                'base_product__authenticity', 'base_product__warranty','base_product__shiping_method',
+                'base_product__category__image', 'base_product__category__slug'
+            )
+        
+        return queryset
 
     def get_serializer_class(self):
         if self.action == 'list':
             return serializers.ProductListSerializer
         return serializers.ProductDetailSerializer
+    
+    def get_serializer_context(self):
+        return {
+            'images': models.ProductImage.objects.all()
+        }
